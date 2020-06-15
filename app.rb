18c6b433e116a1365865ecb61d94a9c7af209fd3
ZIming Mao <ziming.mao@yale.edu>
2020-06-15 14:15:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,25 @@
 class App < Sinatra::Base
 
+	set :views, "views"
+
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+
+		@t = Time.new 
+
+		erb :date
+	end
 
 end
