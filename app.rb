a5ad569712fdb0c27e274f6ea8383d35b7752595
justin_james <jjames11799@gmail.com>
2020-06-15 14:31:11 -0400

    Done, done, and done!

--- app.rb
+++ app.rb
@@ -10,6 +10,16 @@ class App < Sinatra::Base
 		erb :hello
 	end
 
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end 
+
+	get '/date' do 
+		@date = Time.now.strftime("%A, %B %d, %Y") 
+		erb :date 
+	end 
+
 
 end
 
