0c276107e481e403f58efa62faa7ae5035f1265e
CelestialEmperor <mapeiyao@gmail.com>
2020-06-15 19:34:55 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 class App < Sinatra::Base
 
+	require 'date'
+
 	get '/' do
 		erb :index
+	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Date.today
+		erb :date
 	end
 
 
